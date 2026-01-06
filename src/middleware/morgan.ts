5e6268fae905314cf304819e5ecebb3ff7a41d9b
Loïs Bégué <3593197+khatastroffik@users.noreply.github.com>
2026-01-07 00:21:29 +0100

    fix(#9): replace relative with original url in morgan token

--- src/middleware/morgan.ts
+++ src/middleware/morgan.ts
@@ -1,4 +1,4 @@
-import type { IncomingMessage, ServerResponse } from "node:http";
+import type { Request, Response } from "express";
 import morgan from "morgan";
 import { colorize, statusColor } from "../lib/ansi-colors";
 import logger from "../lib/logger";
@@ -9,14 +9,14 @@ const log = logger("MORG");
 /**
  * "coloredStatus" message token returning an ANSI-colored statuscode.
  */
-morgan.token("coloredStatus", (_req, res: ServerResponse<IncomingMessage>): string => {
+morgan.token("coloredStatus", (_req: Request, res: Response): string => {
   return colorize(res.statusCode, statusColor(res.statusCode));
 });
 
 /**
  * "contentLength" message token returning a prettified 'response content-length' using adequate units.
  */
-morgan.token("contentLength", (_req, res: ServerResponse<IncomingMessage>): string | undefined => {
+morgan.token("contentLength", (_req: Request, res: Response): string | undefined => {
   const length = res.getHeaders()["content-length"];
   return `(${prettyBytes(Number(length ?? 0))})`;
 });
@@ -24,8 +24,8 @@ morgan.token("contentLength", (_req, res: ServerResponse<IncomingMessage>): stri
 /**
  * "coloredUrl" message token returning the request URL ANSI-colored according to the response status.
  */
-morgan.token("coloredUrl", (req: IncomingMessage, res: ServerResponse<IncomingMessage>): string | undefined => {
-  return req.url ? colorize(req.url, statusColor(res.statusCode)) : undefined;
+morgan.token("coloredUrl", (req: Request, res: Response): string | undefined => {
+  return (req.originalUrl) ? colorize(req.originalUrl, statusColor(res.statusCode)) : undefined;
 });
 
 /**
